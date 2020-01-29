bdf7244e123b62dc5c6ab8df098499a4997a6433
ravishankar5 <60417239+ravishankar5@users.noreply.github.com>
2020-01-29 11:43:22 +0530

    Create NumberGuessing.py

---
+++ NumberGuessing.py
@@ -0,0 +1,17 @@
+import random
+r=random.randrange(1,100)
+guess = int(input("Enter your guess between 1 to 100"))
+print("You have five chances")
+for i in range(4):
+    if guess>r:
+        print("Your guess is too high")
+        print("try again")
+        guess = int(input("Enter your guess"))
+    elif guess<r:
+        print("Your guess is too low")
+        print("try again")
+        guess = int(input("Enter your guess"))
+    else:
+        print("You WIN")
+        break
+print("Game Over")
